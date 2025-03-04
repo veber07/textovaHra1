fb9bbe202e6d08f431293dcaa60e4977340df313
veber07 <spideroftitan@gmail.com>
2025-03-04 22:52:37 +0100

    Nacteni mistnosti

---
+++ src/Location.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+
+
+    private String name;
+    int id;
+    Map<String, Integer> exits;
+
+    public Location(String name, int id, int nor,int east,int south,int west ) {
+        this.name = name;
+        this.id = id;
+        this.exits = new HashMap<>();
+        exits.put("north", nor);
+        exits.put("east", east);
+        exits.put("south", south);
+        exits.put("west", west);
+
+
+    }
+    public int getExit(String direction){
+        return exits.getOrDefault(direction, 1);
+    }
+}
---
+++ src/Moving.java
@@ -0,0 +1,13 @@
+public class Moving extends Command {
+
+    @Override
+    public String execute() {
+        return "jdi";
+    }
+
+    @Override
+    public boolean exit() {
+        return false ;
+    }
+
+}
